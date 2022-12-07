8d158f42fc00d42eddd84b40aba14a2e00c1ed3f
raphael.montes <raphael.montes@epita.fr>
2022-12-07 09:14:44 +0100

    Day_07: cleanup

--- Day_07/day_07.hs
+++ Day_07/day_07.hs
@@ -21,29 +21,15 @@ getSize :: File -> Int
 getSize (File _ size) = size
 getSize (Dir _ files) = sum  . map getSize $ files
 
-sumLess :: [File] -> Int -> Int
-sumLess [] acc = acc
-sumLess (x : xs) acc
-    | File _ _    <- x = sumLess xs acc
-    | Dir _ files <- x = sumLess xs newAcc + sumLess files 0
-    where size = getSize x
-          newAcc = acc + if size > 100000 then 0 else size
-
-findSmallest :: Int -> Int -> [File] -> Int
-findSmallest _ smallest [] = smallest
-findSmallest toFree smallest (File _ _ : xs) = findSmallest toFree smallest xs
-findSmallest toFree smallest (Dir n files : xs) = findSmallest toFree nsmall xs
-    where nsmall = minimum $ filter (>= toFree)
-                   [
-                    smallest, 
-                    getSize (Dir n files), 
-                    findSmallest toFree smallest files
-                   ]
+getDirSizes :: File -> [Int]
+getDirSizes (File _ _)    = []
+getDirSizes (Dir n files) = 
+    getSize (Dir n files) : foldl (\a d -> a ++ getDirSizes d) [] files
 
 main = do
-    (Dir _ files) <- snd . flip getFilesystem (Dir "/" []) . drop 2 . lines 
-            <$> readFile "input"
-    print $ sumLess files 0
-    let rootSize = getSize (Dir "/" files)
-    let toFree = 30000000 - (70000000 - rootSize)
-    print $ findSmallest toFree rootSize files
+    input <- snd . flip getFilesystem (Dir "/" []) . drop 2 . lines 
+             <$> readFile "input"
+    let dirSizes = getDirSizes input
+    let unused = 70000000 - getSize input
+    print $ sum . filter (<= 100000) $ dirSizes
+    print $ minimum . filter (>= (30000000 - unused)) $ dirSizes
