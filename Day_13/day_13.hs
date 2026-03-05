3e8cdea813f8d80c16c0c415098f23bc290f2b57
Sheinxy <sheinxy@seascow.win>
2026-03-06 00:47:29 +0100

    Day_13: cleanup

--- Day_13/day_13.hs
+++ Day_13/day_13.hs
@@ -10,12 +10,7 @@ instance Ord Packet where
     compare (Number x) (Number y) = compare  x                y
     compare (Number x)         y  = compare (List [Number x]) y
     compare         x  (Number y) = compare  x  (List [Number y])
-    compare (List   []) (List []) = EQ
-    compare (List   []) (List  y) = LT 
-    compare (List    x) (List []) = GT
-    compare (List (x:xs)) (List (y:ys)) | compare x y == LT = LT
-                                        | compare x y == GT = GT
-                                        | otherwise         = compare (List xs) (List ys)
+    compare (List x) (List y)     = compare x y
 
 parsePacket :: String -> Packet
 parsePacket = read . go
@@ -29,7 +24,7 @@ chunk n = takeWhile (not . null) . map (take n) . iterate (drop n)
 
 main = do
     input <- map parsePacket . filter (not . null) . lines <$> readFile "input"
-    print $ sum . map fst . filter ((== True) . snd) . zip [1 .. ] . map (\[a, b] -> a <= b) . chunk 2 $ input
+    print $ sum . map fst . filter snd . zip [1 .. ] . map (\[a, b] -> a <= b) . chunk 2 $ input
     let (div1, div2) = (List [List [Number 2]], List [List [Number 6]])
     let sorted = sort $ div1 : div2 : input
     let indexOf x = fst . head . filter ((== x) . snd) . zip [1 .. ]
